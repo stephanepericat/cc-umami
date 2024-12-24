e69d3f91b7b4694891d5689c2e5dcaff4e27ec11
Stephane P. Pericat <stephanepericat@users.noreply.github.com>
2024-12-24 15:33:35 -0600

    fix headers config (#1)
    Co-authored-by: Stephane Pericat <shortwavlabs@shortwavlabs.local>

--- next.config.js
+++ next.config.js
@@ -68,6 +68,20 @@ const headers = [
     source: TRACKER_SCRIPT,
     headers: trackerHeaders,
   },
+  {
+    // matching all API routes
+    source: '/api/:path*',
+    headers: [
+      { key: 'Access-Control-Allow-Credentials', value: 'true' },
+      { key: 'Access-Control-Allow-Origin', value: '*' },
+      { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+      {
+        key: 'Access-Control-Allow-Headers',
+        value:
+          'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+      },
+    ],
+  },
 ];
 
 const rewrites = [];
